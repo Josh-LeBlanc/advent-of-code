2d8f0c723861260d99621c399658bf363a4f8a84
Josh LeBlanc <joshleblanc@Joshs-MacBook-Pro-2.local>
2024-12-04 12:56:32 -0500

    part 2 done, once again i am a bot

--- 2024/03/2024_03.py
+++ 2024/03/2024_03.py
@@ -3,17 +3,33 @@ import re
 with open("03.in", "r") as f:
     ls = f.readlines()
 
-p = re.compile(r"mul\([0-9]*,[0-9]*\)")
+# with open("03.test", "r") as f:
+#     ls = f.readlines()
+
+p = re.compile(r"(mul\([0-9]+,[0-9]+\))|(do\(\))|(don't\(\))")
 p1 = re.compile(r"[0-9]+")
 r = 0
+do = True
 for s in ls:
     matches = p.findall(s)
-    for m in matches:
-        nm = p1.findall(m)
-        a = 1
-        for y in nm:
-            a *= int(y)
-        r += a
+    for ms in matches:
+        m = ""
+        for mms in ms:
+            if mms != "":
+                m = mms
+                break
+        if m[:2] == "do":
+            if m[2] == "n":
+                do = False
+            else:
+                do = True
+        else:
+            if do and m != "":
+                nm = p1.findall(m)
+                a = 1
+                for y in nm:
+                    a *= int(y)
+                r += a
     
 print(r)
 
